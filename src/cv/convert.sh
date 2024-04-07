7ac116e9a5f0bf31b934aa5ea6ad565ea2769139
Andrei Gramakov <mail@agramakov.me>
2024-04-08 00:37:32 +0200

    Decrease page margins

--- src/cv/convert.sh
+++ src/cv/convert.sh
@@ -1,1 +1,1 @@
-pandoc -f gfm -t html5 --metadata pagetitle="cv.md" --css style.css  cv.md -o ../../cv/Andrei_Gramakov_CV.pdf --pdf-engine-opt=--enable-local-file-access
+pandoc -f gfm -t html5 -V margin-top=3 -V margin-left=3 -V margin-right=3 -V margin-bottom=3 -V papersize=A4 --metadata pagetitle="cv.md" --css style.css  cv.md -o cv.pdf --pdf-engine-opt=--enable-local-file-access
