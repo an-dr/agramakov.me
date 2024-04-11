7c4a37c4851f8c04ae9c2634d987d31067cb3665
Andrei Gramakov <mail@agramakov.me>
2024-04-11 23:54:28 +0200

    Update pdf margins

--- src/cv/convert.sh
+++ src/cv/convert.sh
@@ -1,3 +1,3 @@
 cat cv-header.md cv-body.md > ./cv.md
 
-pandoc -f gfm -t html5 -V margin-top=3 -V margin-left=3 -V margin-right=3 -V margin-bottom=3 -V papersize=A4 --metadata pagetitle="cv.md" --css styles.css  cv.md -o cv.pdf --pdf-engine-opt=--enable-local-file-access
+pandoc -f gfm -t html5 -V margin-top=5 -V margin-left=10 -V margin-right=10 -V margin-bottom=5 -V papersize=A4 --metadata pagetitle="cv.md" --css styles.css  cv.md -o cv.pdf --pdf-engine-opt=--enable-local-file-access
